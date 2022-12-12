fd9f7f2f0ac34d5bb4a3586c2c18ce0e5abd1839
Daniel Neale <dn230726@falmouth.ac.uk>
2022-12-12 16:20:48 +0000

    Added downfall assets

---
+++ Assets/Downfall/Scripts/WheelMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMovement : MonoBehaviour
+{
+    //wheels
+    [SerializeField]
+    private Transform[] wheelOne = new Transform[2];
+    [SerializeField]
+    private Transform[] wheelTwo = new Transform[2];
+    [SerializeField]
+    private Transform[] wheelThree = new Transform[2];
+    [SerializeField]
+    private Transform[] wheelFour = new Transform[2];
+    [SerializeField]
+    private Transform[] wheelFive = new Transform[2];
+}
