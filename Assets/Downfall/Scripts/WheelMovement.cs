1136369e68db17243d25bb6c31e784c1b419a862
Daniel Neale <dn230726@falmouth.ac.uk>
2022-12-13 21:17:45 +0000

    Wheels can now only move in one directon in a turn

--- Assets/Downfall/Scripts/WheelMovement.cs
+++ Assets/Downfall/Scripts/WheelMovement.cs
@@ -19,7 +19,9 @@ public class WheelMovement : MonoBehaviour
     private float thisWheelZ;
     private float pairWheelZ;
     private bool rotating = false;
+    private bool lockRotation = false;
     private bool clockwise;
+    private int lastAngle;
 
 
     private void Start()
@@ -33,12 +35,45 @@ public class WheelMovement : MonoBehaviour
         if (rotating)
         {
             int rotations = Mathf.FloorToInt((Input.mousePosition.x - mouseStartX) / sensitivity);
+
+            if (lockRotation)
+            {
+                if (clockwise && rotations < lastAngle)
+                {
+                    rotations = lastAngle;
+                }
+
+                if (!clockwise && rotations > lastAngle)
+                {
+                    rotations = lastAngle;
+                }
+            }
+
+            lastAngle = rotations;
+
             thisWheel.rotation = Quaternion.Euler(0, 0, thisWheelZ - (rotations * snapAngle));
             pairWheel.rotation = Quaternion.Euler(0, 0, pairWheelZ + (rotations * snapAngle));
+
+            if (!lockRotation)
+            {
+                if ((Input.mousePosition.x - mouseStartX) / sensitivity > 1)
+                {
+                    lockRotation = true;
+                    clockwise = true;
+                }
+
+                else if ((Input.mousePosition.x - mouseStartX) / sensitivity < -1)
+                {
+                    lockRotation = true;
+                    clockwise = false;
+                }
+            }
 
             if (Input.GetMouseButton(0))
             {
                 rotating = false;
+                lockRotation = false;
+                lastAngle = 0;
                 gameCont.ToggleButtons(true);
             }
         }
