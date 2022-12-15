f658feb6bd47d8e9eb1066d03c3843dd37af06e0
Daniel Neale <dn230726@falmouth.ac.uk>
2022-12-15 16:07:21 +0000

    Started counter movement

---
+++ Assets/Downfall/Scripts/CounterMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform test;
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
