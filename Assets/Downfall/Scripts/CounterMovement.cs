5efcd7ffe9b92bc68807b212ee59b1dfe343e8be
Daniel Neale <dn230726@falmouth.ac.uk>
2023-01-06 12:57:58 +0000

    Counter movement now collision based

--- Assets/Downfall/Scripts/CounterMovement.cs
+++ Assets/Downfall/Scripts/CounterMovement.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class CounterMovement : MonoBehaviour
 {
-    [SerializeField]
-    private Transform test;
+    private float detectionRange = 1f;
+    private Transform parent;
 
 
     private void FixedUpdate()
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, detectionRange);
+        Debug.DrawRay(transform.position, transform.up * detectionRange, Color.blue);
+
+        if (hit.transform.parent.GetComponent<WheelMovement>())
+        {
+            NewParent(hit.transform);
+        }
+    }
+
+
+    private void NewParent(Transform newParent)
+    {
+        parent = newParent;
+        transform.parent = parent;
     }
 }
